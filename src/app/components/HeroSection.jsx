6ffe769f4d55e939f68f6786c53014d51e649c1f
Hrishi Shah <114273264+Israfil001@users.noreply.github.com>
2024-06-03 17:43:22 -0600

    Basic project dependencies installed and ready
    Some of the landing page has been tweaked and worked on.

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return <section>
+    <h1 className = "text-white">Hi, I'm Hrishi</h1>
+  </section>
+};
+
+export default HeroSection; 
