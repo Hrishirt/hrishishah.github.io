c0341e13e877a310150277e5258464f3e66c1316
Hrishi Shah <shrishi291002@gmail.com>
2024-06-10 02:29:46 -0600

    Fixed navbar
    Made a navLink function and passed the About, Experience, Projects and Contact links into it.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import Link from 'next/link'
+import NavLink from './NavLink'
+ 
+
+const NavLinks = 
+[
+    {
+        title: "About",
+        path: "#about"
+    },
+    {
+        title: "Experience",
+        path: "#experience"
+    },
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+    {
+        title: "Contact",
+        path: "#about"
+    }
+
+]
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href={"/"} className="text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4]">HRISHI SHAH</Link>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul>
-                    <li>
-                        <Link href={'#about'} 
-                        className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'>
-                            About
-                        </Link>
-                    </li>
+    <nav className='fixed top-0 right-0 left-0 bg-[#121212] bg-opacity-90'>
+        <div className="flex flex-wrap items-center justify-between mx-auto px-10 py-3">
+            <Link href={"/"} className="text-xl md:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4]">SHAH</Link>
+            <div className="menu block md:w-auto" id="navbar">
+                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8'>
+                    {
+                        NavLinks.map((link, index) => (
+                            <li>
+                                <NavLink href={link.path} title = {link.title}/>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
