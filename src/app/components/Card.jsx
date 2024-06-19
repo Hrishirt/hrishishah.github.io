403106a44bbb41759947e59350c791dbafb85423
Hrishi Shah <114273264+Hrishirt@users.noreply.github.com>
2024-06-19 12:26:31 -0600

    Fixed cards
    made the cards shrink/grow relative to the text within them, changed styling, and added the blue theme.

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,10 +2,15 @@
 export default function Card({ children, imageSource, ...props }) {
     return (
         <div {...props} className="relative w-full sm:w-80 max-w-xs overflow-hidden rounded-2xl shadow-lg group">
-            <img src={imageSource} alt="" className="w-full h-full object-cover transition-transform group-hover:scale-110" />
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-transparent">
-                <div className="p-4 text-white">{children}</div>
+            <div className="relative w-full h-48">
+                <img src={imageSource} alt="" className="absolute inset-0 w-full h-full object-cover transition-transform group-hover:scale-110" />
+            </div>
+            <div className="relative bg-gradient-to-t from-black/60 to-transparent p-4">
+                <div className="text-[#ADB7Be]">
+                    {children}
+                </div>
             </div>
         </div>
     );
 }
+
--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -6,17 +6,19 @@ const Experience = () => {
     <main className='App p-4'>
     <h2 className='text-4xl font-bold flex justify-center gap-4 text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4] py-12'>Experience</h2>
       <div className="flex flex-wrap justify-center gap-4">
-        <Card imageSource={'/images/computer.jpg'}>
-          <h3 className='text-xl font-bold mb-2'>Dev ops Engineer Intern</h3> 
+        <Card imageSource={'/images/compScience.jpg'}>
+          <h3 className='text-xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4]'>Dev ops Engineer Intern</h3> 
           <p>
             Developed and maintained the company using API's and DigitalOcean. 
             ran different tests to make sure different components of the server were working 
             Enhanced architecture using containerization to improve application performance.
+            
+            
           </p>   
         </Card>
 
-        <Card imageSource={'/images/computer.jpg'}>
-          <h3 className='text-xl font-bold mb-2'>Software Engineer Team Leader Intern</h3> 
+        <Card imageSource={'/images/compScience.jpg'}>
+          <h3 className='text-xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4]'>Software Engineer Team Leader Intern</h3> 
           <p>
           Led a team of 5 interns to solve various problems and make a profit for the Company. Manually built 
           sensors to capture data. Used languages such as C, Python in order to develop a data collection system. 
@@ -24,7 +26,7 @@ const Experience = () => {
         </Card>
 
         <Card imageSource={'/images/compScience.jpg'}>
-          <h3 className='text-xl font-bold mb-2'>Computing Science Teaching Assistant</h3> 
+          <h3 className='text-xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#373B44] to-[#4286f4]'>Computing Science Teaching Assistant</h3> 
           <p>
           Created assignments that matched course topics. 
           I held one-on-one help sessions to support their understanding and success. 
